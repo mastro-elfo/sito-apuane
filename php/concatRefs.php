50af3362c811cefc415d70f6be1344f83676a475
mastro-elfo <francesco.209@gmail.com>
2021-03-13 17:58:44 +0100

    Fix concat if empty

--- php/concatRefs.php
+++ php/concatRefs.php
@@ -2,6 +2,9 @@
 {
     return implode(", ", array_map(function ($item) use ($href) {
         $parts = explode("/", $item);
-        return "<a href='$href?id=$parts[1]'>$parts[0]</a>";
+        if (count($parts) > 1) {
+            return "<a href='$href?id=$parts[1]'>$parts[0]</a>";
+        }
+        return "";
     }, $list));
 }
