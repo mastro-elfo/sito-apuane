4b94bc4c135714cbfb545ea677a3f21b072425e3
mastro-elfo <francesco.209@gmail.com>
2021-03-14 18:34:49 +0100

    Remove comma between tags

--- php/concatTags.php
+++ php/concatTags.php
@@ -1,7 +1,7 @@
 <?php
 function concatTags($tags)
 {
-    return implode(", ", array_map(function ($tag) {
+    return implode(" ", array_map(function ($tag) {
         $parts = explode("/", $tag);
         if (count($parts) >= 3) {
             return "<span class='tag' style='background-color:$parts[2];color:$parts[3]'>$parts[1]</span>";
