21ae47b7257460f6e9f876016f5ad497e00d3237
mastro-elfo <francesco.209@gmail.com>
2021-03-16 13:33:51 +0100

    Fix require and bugs

--- php/img.php
+++ php/img.php
@@ -1,21 +1,29 @@
 <?php
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
-require_once "../ajax/database.php";
+require_once "../php/database.php";
+
+function broken(){
+  header("Content-Type: image/*");
+  echo file_get_contents("../imgs/broken.png");
+  exit;
+}
 
 $id = $_GET["id"];
 $tableName = $_GET["table"];
 
 if (!$id) {
-    echo file_get_contents("../imgs/broken.png");
-    exit;
+    broken();
 }
 
 // Open db connection
 $db = open_db();
+
 // On error return null
 if (!$db) {
-    echo file_get_contents("../imgs/broken.png");
-    exit;
+    broken();
 }
 
 $ret = $db->query("
@@ -23,10 +31,16 @@ $ret = $db->query("
   FROM `$tableName`
   WHERE id = '$id'
 ");
-$image = $ret->fetch_assoc();
-if (!$image) {
-    echo file_get_contents("../imgs/broken.png");
-    exit;
+
+if(!$ret){
+  broken();
 }
 
+$image = $ret->fetch_assoc();
+
+if (!$image) {
+    broken();
+}
+
+header("Content-Type: image/*");
 echo $image["image"];
