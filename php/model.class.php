f7045862a239c89d3d19f07f5fa2a5f8d876f90e
mastro-elfo <francesco.209@gmail.com>
2021-03-16 14:44:27 +0100

    Improve model class

--- php/model.class.php
+++ php/model.class.php
@@ -1,44 +1,73 @@
 <?php
 
-require_once __DIR__."/database.php";
+require_once __DIR__ . "/database.php";
 
-class Model {
-  //
+class Model
+{
+    // mysqli database object
     protected $_db = null;
+    // table name
+    protected $_table = null;
+    // row id
+    protected $_id = null;
 
-    public function __construct()
+    public function __construct($id = null)
     {
+        $this->_id = $id;
         $this->_db = open_db();
     }
 
     public function __get($key)
     {
         $_key = "_$key";
-        return $this->$_key;
-    }
-
-    public function __set($key, $value)
-    {
-        if (in_array($key, ["name", "article", "title", "description", "image"])) {
-            $_key        = "_$key";
-            $this->$_key = $value;
+        if (property_exists($this, $_key)) {
+            return $this->$_key;
         }
+        return null;
     }
 
-    public function create(){
-      return false;
+    // public function __set($key, $value)
+    // {
+    //     // TODO: E' specifico per places
+    //     if (in_array($key, ["name", "article", "title", "description", "image"])) {
+    //         $_key        = "_$key";
+    //         $this->$_key = $value;
+    //     }
+    // }
+
+    public function query($q)
+    {
+        if ($this->_db) {
+            return $this->_db->query($q);
+        }
+        return false;
     }
 
-    public function read(){
-      return null;
+    public function create()
+    {
+        return false;
     }
 
-    public function update(){
-      return false;
+    public function read()
+    {
+        return null;
     }
 
-    public function delete(){
-      return false;
+    public function update()
+    {
+        return false;
+    }
+
+    public function delete()
+    {
+        if ($this->_db) {
+            $ret = $this->_db->query("
+            UPDATE `$this->_table`
+            SET deleted = 1
+            WHERE id = '$this->_id'
+          ");
+            return $ret;
+        }
+        return false;
     }
 }
- ?>
--- php/place.class.php
+++ php/place.class.php
@@ -3,7 +3,9 @@ require_once __DIR__ . "/model.class.php";
 
 class Place extends Model
 {
-    protected $_id          = null;
+    // Set table name
+    protected $_table = "places";
+    // Set properties
     protected $_name        = null;
     protected $_article     = null;
     protected $_title       = null;
@@ -16,8 +18,7 @@ class Place extends Model
 
     public function __construct($id = null, $name = null, $article = null, $title = null, $description = null, $image = null)
     {
-        parent::__construct();
-        $this->_id          = $id;
+        parent::__construct($id);
         $this->_name        = $name;
         $this->_article     = $article;
         $this->_title       = $title;
@@ -25,121 +26,97 @@ class Place extends Model
         $this->_image       = $image;
     }
 
-    public function create()
-    {
-
-    }
+    // public function create()
+    // {
+    //
+    // }
 
     public function read()
     {
-        if ($this->_db) {
-            $ret = $this->_db->query("
-              SELECT p.name, p.article, p.title, p.description, p.image, p.uDateTime
-              FROM places p
-              WHERE p.id = '$this->_id'
-                AND p.deleted = 0
-            ");
-            if ($ret) {
-                $place              = $ret->fetch_assoc();
-                $this->_name        = $place["name"];
-                $this->_article     = $place["article"];
-                $this->_title       = $place["title"];
-                $this->_description = $place["description"];
-                $this->_image       = $place["image"];
-                $this->_uDateTime   = $place["uDateTime"];
-                return true;
-            }
+        $ret = $this->query("
+          SELECT p.name, p.article, p.title, p.description, p.image, p.uDateTime
+          FROM `$this->_table` p
+          WHERE p.id = '$this->_id'
+            AND p.deleted = 0
+        ");
+        if ($ret) {
+            $place              = $ret->fetch_assoc();
+            $this->_name        = $place["name"];
+            $this->_article     = $place["article"];
+            $this->_title       = $place["title"];
+            $this->_description = $place["description"];
+            $this->_image       = $place["image"];
+            $this->_uDateTime   = $place["uDateTime"];
+            return true;
         }
         return false;
     }
 
     public function update()
     {
-        if ($this->_db) {
-            $ret = $this->_db->query("
-            UPDATE places
-            SET
-              name = '$this->_name',
-              article = '$this->_article',
-              title = '$this->_title',
-              description = '$this->_description',
-              image = '$this->_image'
-            WHERE id = '$this->_id'
-          ");
-            return $ret;
-        }
-        return false;
-    }
-
-    public function delete()
-    {
-        if ($this->_db) {
-            $ret = $this->_db->query("
-              UPDATE places
-              SET deleted = 1
-              WHERE id = '$this->_id'
-            ");
-            return $ret;
-        }
-        return false;
+        $ret = $this->query("
+          UPDATE `$this->_table`
+          SET
+            name = '$this->_name',
+            article = '$this->_article',
+            title = '$this->_title',
+            description = '$this->_description',
+            image = '$this->_image'
+          WHERE id = '$this->_id'
+        ");
+        return $ret;
     }
 
     public function readRelated()
     {
-        if ($this->_db) {
-            $ret = $this->_db->query("
-              SELECT
-                o.id, o.name
-              FROM places o
-              INNER JOIN places_places pp ON pp.idTo = o.id
-              WHERE pp.idFrom = '$this->_id'
-                AND pp.deleted = 0
-                AND o.deleted = 0
-            ");
-            if ($ret) {
-                $this->_related = $ret->fetch_all(MYSQLI_ASSOC);
-                return true;
-            }
+        $ret = $this->query("
+          SELECT
+            o.id, o.name
+          FROM places o
+          INNER JOIN places_places pp ON pp.idTo = o.id
+          WHERE pp.idFrom = '$this->_id'
+            AND pp.deleted = 0
+            AND o.deleted = 0
+        ");
+        if ($ret) {
+            $this->_related = $ret->fetch_all(MYSQLI_ASSOC);
+            return true;
         }
         return false;
     }
 
     public function readTags()
     {
-        if ($this->_db) {
-            $ret = $this->_db->query("
-              SELECT
-                t.id, t.name, t.color, t.textColor
-              FROM tags t
-              INNER JOIN places_tags pt ON pt.idTag = t.id
-              WHERE
-                    pt.idPlace = '$this->_id'
-                AND t.deleted = 0
-                AND pt.deleted = 0
-            ");
-            if ($ret) {
-                $this->_tags = $ret->fetch_all(MYSQLI_ASSOC);
-                return true;
-            }
+        $ret = $this->query("
+          SELECT
+            t.id, t.name, t.color, t.textColor
+          FROM tags t
+          INNER JOIN places_tags pt ON pt.idTag = t.id
+          WHERE
+                pt.idPlace = '$this->_id'
+            AND t.deleted = 0
+            AND pt.deleted = 0
+        ");
+        if ($ret) {
+            $this->_tags = $ret->fetch_all(MYSQLI_ASSOC);
+            return true;
         }
         return false;
     }
 
     public function readAttributes()
     {
-        if ($this->_db) {
-            $ret = $this->_db->query("
-              SELECT
-                a.name, a.value, a.after
-              FROM attributes a
-              WHERE
-                    a.idPlace = '$this->_id'
-                AND a.deleted = 0
-            ");
-            if ($ret) {
-                $this->_attributes = $ret->fetch_all(MYSQLI_ASSOC);
-                return true;
-            }
+        $ret = $this->query("
+          SELECT
+            a.name, a.value, a.after
+          FROM attributes a
+          WHERE
+                a.idPlace = '$this->_id'
+            AND a.deleted = 0
+        ");
+        if ($ret) {
+            $this->_attributes = $ret->fetch_all(MYSQLI_ASSOC);
+            return true;
         }
         return false;
     }
