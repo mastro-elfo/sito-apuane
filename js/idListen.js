62c155384d44e31b02a65eab804c42b5636bd8c3
mastro-elfo <francesco.209@gmail.com>
2021-03-17 18:38:13 +0100

    Remove unused files

--- js/idListen.js
+++
@@ -1,9 +0,0 @@
-function idListen(id, event, callback, ...params) {
-  let element = document.getElementById(id);
-  if (element) {
-    element.addEventListener(event, function (e) {
-      e.preventDefault();
-      callback(...params);
-    });
-  }
-}
