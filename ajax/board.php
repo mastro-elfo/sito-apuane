bda7ad1f396c67d99d3825155c6f674abec227cd
mastro-elfo <francesco.209@gmail.com>
2021-03-18 12:09:09 +0100

    Add action answer

--- ajax/board.php
+++ ajax/board.php
@@ -5,6 +5,7 @@ error_reporting(E_ALL);
 
 session_start();
 
+require_once "../php/answer.class.php";
 require_once "../php/board.class.php";
 
 function create($title, $content)
@@ -16,8 +17,25 @@ function create($title, $content)
     return null;
 }
 
+function answer($boardId, $content)
+{
+    if (isset($_SESSION["user"])) {
+        $answer = new Answer(
+            null,
+            $content,
+            ["id" => null],
+            ["id" => $boardId],
+            $_SESSION["user"]
+        );
+        return $answer->create();
+    }
+    return null;
+}
+
 if ($_POST["action"] == "create") {
     echo json_encode(create($_POST["title"], $_POST["content"]));
+} elseif ($_POST["action"] == "answer") {
+    echo json_encode(answer($_POST["boardId"], $_POST["content"]));
 } else {
     echo json_encode(null);
 }
