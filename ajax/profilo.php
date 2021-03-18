5c2b1ddd54f0963219126b78880565204a0afec4
mastro-elfo <francesco.209@gmail.com>
2021-03-18 21:19:37 +0100

    Add delete function

--- ajax/profilo.php
+++ ajax/profilo.php
@@ -9,24 +9,44 @@ require_once "../php/user.class.php";
 
 function update($name, $email)
 {
-    // Get user id from session
-    $userId = $_SESSION["user"]["id"];
-    // Create new User
-    $user = new User($_SESSION["user"]["id"], $email, $name);
-    // Update user
-    $ret = $user->update();
-    if(!$ret) {
-      return null;
+    if (isset($_SESSION["user"]["id"])) {
+        // Get user id from session
+        $userId = $_SESSION["user"]["id"];
+        // Create new User
+        $user = new User($userId, $email, $name);
+        // Update user
+        $ret = $user->update();
+        if (!$ret) {
+            return null;
+        }
+        // Reload user
+        $u = $user->read();
+        // Update session variable
+        if ($u) {
+            $_SESSION["user"] = $u;
+        }
+        return $u;
     }
-    // Reload user
-    $u = $user->read();
-    // Update session variable
-    if ($u) {
-        $_SESSION["user"] = $u;
+    http_response_code(401);
+    return false;
+}
+
+function delete()
+{
+    if (isset($_SESSION["user"]["id"])) {
+        // Get user id from session
+        $userId = $_SESSION["user"]["id"];
+        $user   = new User($userId);
+        return $user->delete();
     }
-    return $u;
+    http_response_code(401);
+    return false;
 }
 
 if ($_POST["action"] == "update") {
     echo json_encode(update($_POST["name"], $_POST["email"]));
+} elseif ($_POST["action"] == "delete") {
+    echo json_encode(delete());
+} else {
+    echo json_encode(null);
 }
