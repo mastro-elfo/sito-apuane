dc9fb2219632a4e799d2f66aa15f103279a7cceb
mastro-elfo <francesco.209@gmail.com>
2021-03-18 21:25:34 +0100

    Add delete function

--- js/profilo.js
+++ js/profilo.js
@@ -48,4 +48,36 @@ $(function () {
       },
     });
   });
+
+  $("#delete").on("click", function () {
+    setResponse("info", "Richiesta conferma...");
+    if (
+      confirm(
+        "Sei sicuro di voler eliminare il tuo account. Tutti i dati saranno cancellati e non sarà più possibile ripristinarli.\n\nSe confermi non sarà più possibile accedere all'area privata del sito."
+      )
+    ) {
+      setResponse("info", "Elimino l'account...");
+      $.ajax("ajax/profilo.php", {
+        method: "POST",
+        data: { action: "delete" },
+        success: function (r) {
+          try {
+            // Check if response is valid JSON
+            JSON.parse(r);
+            setResponse("success", "Account eliminato");
+            setTimeout(() => {
+              location.href = "./";
+            }, 500);
+          } catch (e) {
+            setResponse("error", `${e.name}, ${e.message}`);
+          }
+        },
+        error: function () {
+          setResponse("error", "Errore AJAX");
+        },
+      });
+    } else {
+      setResponse("info", "Operazione annullata");
+    }
+  });
 });
