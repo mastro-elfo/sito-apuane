9f0fd7cbaba4d6ba0e91291fa1d96ccf8b9ee36c
mastro-elfo <francesco.209@gmail.com>
2021-03-21 09:48:09 +0100

    Improve queries

--- oop/place.class.php
+++ oop/place.class.php
@@ -14,12 +14,36 @@ class Place extends Model
         parent::__construct("places", $id);
     }
 
+    public function read($columns = null){
+      return parent::read(
+        is_null($columns)
+        ? ["id", "name", "title", "description", "article", "!isnull(image) as image", "uDateTime"]
+        : $columns
+      );
+    }
+
     public function search($string)
     {
-        $query = (new Query)
-            ->select(["id", "name"])
-            ->from($this->_table)
-            ->where("name LIKE '%$string%'");
+        // $query = (new Query)
+        //     ->select(["id", "name"])
+        //     ->from($this->_table)
+        //     ->where("(name LIKE '%$string%' OR description LIKE '%$string%')")
+        //     ->and("deleted = 0");
+        $query = "
+          SELECT
+            p.id, p.name, p.title,
+            (SELECT
+              GROUP_CONCAT(t.name, '/', t.color, '/', t.textColor
+                SEPARATOR ',')
+            FROM tags t
+            INNER JOIN places_tags pt ON pt.idTag = t.id
+            WHERE pt.idPlace = p.id
+            ORDER BY t.name ASC) AS tags
+          FROM `$this->_table` p
+          WHERE p.deleted = 0
+            ".($string ? "AND name LIKE '%$string%'" : "")."
+          ORDER BY p.name ASC
+        ";
         $ret = $this->_db->query($query);
         if ($ret) {
             return $ret->fetch_all(MYSQLI_ASSOC);
