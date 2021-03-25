a8951fead04a3cbed974771ba01d3e2f9ac4eef6
mastro-elfo <francesco.209@gmail.com>
2021-03-25 09:59:46 +0100

    Fix colors

--- js/snackbar.js
+++ js/snackbar.js
@@ -1,15 +1,22 @@
 const textColors = {
   info: "#FFFFFF",
   success: "#FFFFFF",
-  warning: "#000",
+  warning: "#323232",
   error: "#FFFFFF",
 };
 
 const backgroundColors = {
   info: "#08f",
   success: "#080",
-  warning: "orange",
-  error: "crimson",
+  warning: "#ffa500",
+  error: "#dc143c",
+};
+
+const actionTextColors = {
+  info: "#048",
+  success: "#040",
+  warning: "#7f5200",
+  error: "#6e0A1e",
 };
 
 function snackbar(text, severity = "info") {
@@ -17,7 +24,8 @@ function snackbar(text, severity = "info") {
     text,
     pos: "bottom-center",
     actionText: "Chiudi",
-    textColor: textColors[severity],
-    backgroundColor: backgroundColors[severity],
+    textColor: textColors[severity] || "#FFFFFF",
+    backgroundColor: backgroundColors[severity] || "#323232",
+    actionTextColor: actionTextColors[severity] || "#4CAF50",
   });
 }
