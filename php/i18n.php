79e7daddce29136a067dea0c2da9141409759c83
mastro-elfo <francesco.209@gmail.com>
2021-05-21 19:14:48 +0200

    Get language from browser

--- php/i18n.php
+++ php/i18n.php
@@ -1,8 +1,16 @@
 <?php
 
+$lng = "it";
+if (array_key_exists("HTTP_ACCEPT_LANGUAGE", $_SERVER)) {
+    $accept_lng = substr($_SERVER["HTTP_ACCEPT_LANGUAGE"], 0, 2);
+    if (in_array($accept_lng, ["it", "en", "de"])) {
+        $lng = $accept_lng;
+    }
+}
+
 function i18n($page)
 {
-    $lng  = "it";
+    global $lng;
     $i18n = parse_ini_file("locales/$page.ini", true);
     return $i18n[$lng];
 }
