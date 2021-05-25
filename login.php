3a2b0f9ba580e2ac44f2c51d5fae287c84632d90
mastro-elfo <francesco.209@gmail.com>
2021-05-25 11:16:12 +0200

    i18n login

--- login.php
+++ login.php
@@ -1,3 +1,11 @@
+<?php
+
+require_once "php/i18n.php";
+
+$i18n = i18n("login");
+
+?>
+
 <!DOCTYPE html>
 <html lang="it" dir="ltr">
   <head>
@@ -5,13 +13,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta
       name="description"
-      content="Pagina di accesso all'area riservata del sito."
+      content="<?=$i18n["description"]?>"
     />
     <link rel="stylesheet" href="css/style.css" />
     <link rel="stylesheet" href="css/accesso.css" />
     <link rel="stylesheet" href="lib/js/SnackBar-master/dist/snackbar.min.css">
     <link rel="icon" href="favicon.png" type="image/png"/>
-    <title>Accesso al sito</title>
+    <title><?=$i18n["title"]?></title>
     <script src="lib/js/jquery-3.6.0.js"></script>
     <script src="lib/js/SnackBar-master/dist/snackbar.min.js"></script>
     <script src="js/snackbar.js"></script>
@@ -23,7 +31,7 @@
     <main>
       <form id="login">
         <fieldset>
-          <label for="username">Username</label>
+          <label for="username"><?=$i18n["username"]?></label>
           <input
             type="email"
             id="username"
@@ -31,7 +39,7 @@
             value=""
             autofocus
             class="mb1"/>
-          <label for="password">Password</label>
+          <label for="password"><?=$i18n["password"]?></label>
           <input
             type="password"
             id="password"
@@ -39,11 +47,11 @@
             value=""
             class="mb1"/>
           <div class="button-container">
-            <button type="submit" class="bSuccess">Login</button>
+            <button type="submit" class="bSuccess"><?=$i18n["login"]?></button>
             <a
               href="registrazione.php"
               class="button bWarning"
-              >Registrati</a>
+              ><?=$i18n["signup"]?></a>
           </div>
         </fieldset>
       </form>
